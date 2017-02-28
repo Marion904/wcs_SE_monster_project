81a683a8d2ab21f11e7b0551ec4cdf20395939c3
kerunix <lafont.pierre.wcs@gmail.com>
2017-02-28 13:10:28 +0100

    Intents working on main page

--- app/src/main/java/com/example/wilder/app_wcs_se_monster_project/MainActivity.java
+++ app/src/main/java/com/example/wilder/app_wcs_se_monster_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wilder.app_wcs_se_monster_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button see_monster_button = (Button) findViewById(R.id.see_monster_button);
+        Button add_monster_button = (Button) findViewById(R.id.add_monster_button);
+
+
+        see_monster_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent see_monster_intent =  new Intent(MainActivity.this, MonsterCollectionActivity.class);
+                startActivity(see_monster_intent);
+            }
+        });
+
+
+        add_monster_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent add_monster_intent = new Intent(MainActivity.this, CreateMonsterActivity.class);
+                startActivity(add_monster_intent);
+            }
+        });
     }
 }
