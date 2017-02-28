8f02ab84f66028704e418e424fcf4c386428ac52
kerunix <lafont.pierre.wcs@gmail.com>
2017-02-28 11:31:34 +0100

    finished layout for create monster page

---
+++ app/src/main/java/com/example/wilder/app_wcs_se_monster_project/CreateMonsterActivity.java
@@ -0,0 +1,13 @@
+package com.example.wilder.app_wcs_se_monster_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMonsterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_monster);
+    }
+}
